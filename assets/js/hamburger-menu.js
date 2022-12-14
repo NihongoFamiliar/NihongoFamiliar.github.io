2c071cef4f50fa6c390904967c1fbf413d95b5e5
NihongoFamiliar <120069956+NihongoFamiliar@users.noreply.github.com>
2022-12-14 01:09:08 -0500

    Update hamburger-menu.js

--- assets/js/hamburger-menu.js
+++ assets/js/hamburger-menu.js
@@ -1,19 +1,46 @@
 //	Hamburger Menu: Open
 const openHamburgerMenu = (e) => {
-	document.getElementById('nav-menu').classList.remove('closed');
-	document.getElementById('nav-menu').classList.add('open');
-};
-document.getElementById('nav-menu-hamburger').addEventListener('click', openHamburgerMenu);
-//	Hamburger Menu: Close
-const closeHamburgerMenu = (e) => {
-	const canHasHamburger = window.innerWidth <= 768;
-	
-	if (!canHasHamburger) {
-		document.getElementById('nav-menu').classList.remove('open');
-		document.getElementById('nav-menu').classList.remove('closed');
-	} else if (e.target.id === 'nav-menu') {
-		document.getElementById('nav-menu').classList.add('closed');
-		document.getElementById('nav-menu').classList.remove('open');
+	e.preventDefault();
+	e.stopPropagation();
+
+	const navMenuEl = document.getElementById('nav-menu');
+	if (navMenuEl) {
+		navMenuEl.classList.remove('closed');
+		navMenuEl.classList.add('open');
+		const listItem1El = navMenuEl.querySelector('ul > li:first-child > a');
+		if (listItem1El) { listItem1El.focus(); }
 	}
 };
-document.getElementById('nav-menu').addEventListener('click', closeHamburgerMenu);
+const hamburgerEl = document.getElementById('nav-menu-hamburger');
+if (hamburgerEl) { hamburgerEl.addEventListener('click', openHamburgerMenu); }
+
+//	Hamburger Menu: Close
+const closeHamburgerMenu = (e) => {
+	if ((
+			e.type === 'focusout' &&
+			e.relatedTarget && e.relatedTarget.offsetParent &&
+			e.relatedTarget.offsetParent === e.currentTarget
+		) || (e.type === 'click' && e.target !== e.currentTarget)
+	) { return; }
+
+	const navMenuEl = document.getElementById('nav-menu');
+	if (navMenuEl) {
+		const canHasHamburger = window.innerWidth <= 768;
+
+		if (!canHasHamburger) {
+			navMenuEl.classList.remove('open');
+			navMenuEl.classList.remove('closed');
+		} else {
+			navMenuEl.classList.add('closed');
+			navMenuEl.classList.remove('open');
+		}
+	}
+	const hamburgerEl = document.getElementById('nav-menu-hamburger');
+	if (hamburgerEl) { hamburgerEl.focus(); }
+};
+
+const navMenuEl = document.getElementById('nav-menu');
+if (navMenuEl) {
+	navMenuEl.addEventListener('click', closeHamburgerMenu);
+	navMenuEl.addEventListener('focusout', closeHamburgerMenu);
+}
